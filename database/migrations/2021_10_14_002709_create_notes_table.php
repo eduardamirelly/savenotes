dce78e9555570d4ced9691113eadb916d7647042
eduardamirelly <mirelly.eduarda404@gmail.com>
2021-10-13 21:51:15 -0300

    :construction: add model Note and migrations: notes->table and marks->table

---
+++ database/migrations/2021_10_14_002709_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('color', 30);
+            $table->foreignId('mark_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
