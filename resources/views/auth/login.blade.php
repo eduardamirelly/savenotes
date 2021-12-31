04d21ae80238cc43d8667070cca7ecec53183d40
eduardamirelly <mirelly.eduarda404@gmail.com>
2021-12-30 21:26:31 -0300

    :construction: moving interface settings to jetstream templates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,49 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.base')
 
-        <x-jet-validation-errors class="mb-4" />
+@section('title', 'Login')
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+@section('content')
+
+<!-- CONTENT SING IN -->
+
+    <div class="content-center display-flex align-items-center justify-content-center">
+        <div class="display-flex align-items-start justify-content-center">
+            <div id="img-input">
+                <img src="./images/img.jpg" alt="img" width="600px">
             </div>
-        @endif
+            <div class="form-input display-flex justify-content-center align-items-center">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                <x-jet-validation-errors class="mb-4" />
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                @if (session('status'))
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ session('status') }}
+                    </div>
                 @endif
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="display-flex flex-direction-column align-items-start justify-content-center">
+                        <h2>Sing In</h2>
+                        <input class="input-digit" id="email" type="email" name="email" :value="old('email')" placeholder="email" required autofocus>
+                        <input class="input-digit" id="password" type="password" name="password" placeholder="password" autocomplete="current-password" required>
+
+                        <button class="button-green button-input" type="submit">Login</button>
+                        
+                        <p>Não tem uma conta? <a href="#">SingUp</a></p>
+
+                        @if (Route::has('password.request'))
+                            <p>
+                                Esqueceu sua senha? 
+                                <a href="{{ route('password.request') }}">Clique aqui</a>
+                            </p>
+                        @endif
+                    </div>
+                </form>
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+    </div>
+
+<!-- END CONTENT SING IN -->
+
+@endsection
